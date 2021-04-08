d41ee28f8532043d70085b9028bc4c29d55357d6
nikinov <novellenicholas@gmail.com>
2021-04-08 17:24:01 +0200

    Detection Library V01

---
+++ Detector.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ML;
+using Microsoft.ML.OnnxRuntime;
+using System.Drawing;
+using System.IO;
+
+namespace ClassLibraryDetector
+{
+    public class Detector
+    {
+        private PredictionEngine<ModelInput, ModelPrediction> predictionEngine = null;
+        private string[] labels = null;
+
+        public void Init(string modelDir, string labelsDir)
+        {
+            var context = new MLContext();
+
+            var emptyData = new List<ModelInput>();
+
+            var data = context.Data.LoadFromEnumerable(emptyData);
+            
+            var pipeline = context.Transforms.ResizeImages(resizing: Microsoft.ML.Transforms.Image.ImageResizingEstimator.ResizingKind.Fill,
+                outputColumnName: "data", imageHeight: 300, imageWidth: 300,
+                inputColumnName: nameof(ModelInput.image))
+                .Append(context.Transforms.ExtractPixels(outputColumnName: "data"))
+                .Append(context.Transforms.ApplyOnnxModel(modelFile: modelDir, outputColumnName: "model_output", inputColumnName: "data"));
+
+            var model = pipeline.Fit(data);
+
+            predictionEngine = context.Model.CreatePredictionEngine<ModelInput, ModelPrediction>(model);
+
+            labels = File.ReadAllLines(labelsDir);
+        }
+
+        public float[] GetPrediction(Bitmap bitmapImage)
+        {
+            try
+            {
+                var prediction = predictionEngine.Predict(new ModelInput { image = bitmapImage });
+                return prediction.PredictedLabels;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("ERROR: You have probably forgot to call Detector.Init(modelDir) at the start of the program!");
+                throw;
+            }
+            return new float[0];
+        }
+
+        public string GetPredictionLabel(Bitmap bitmapImage)
+        {
+            try
+            {
+                var prediction = predictionEngine.Predict(new ModelInput { image = bitmapImage });
+
+                return labels[prediction.PredictedLabels.ToList().IndexOf(prediction.PredictedLabels.Max())];
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("ERROR: You have probably forgot to call Detector.Init(modelDir) at the start of the program!");
+                throw;
+            }
+            return "None";
+        }
+    }
+}
---
+++ ModelInput.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.ML.Transforms.Image;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryDetector
+{
+    class ModelInput
+    {
+        [ImageType(300, 300)]
+        public Bitmap image { get; set; }
+    }
+}
