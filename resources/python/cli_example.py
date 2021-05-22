10c82b055682ab15a3ee00b58e5881ce3a5d5261
nikinov <novellenicholas@gmail.com>
2021-05-23 00:03:07 +0200

    working varsion

--- resources/python/cli_example.py
+++ resources/python/cli_example.py
@@ -7,13 +7,17 @@ def main():
     :return: None
     """
     parser = ap.ArgumentParser()
-    parser.add_argument("-dp","--data_path", help="make model", type=str)
-    parser.add_argument("-op", "--out_path", help="define output path", type=str)
+    parser.add_argument("dp", help="make model", type=str)
+    parser.add_argument("op", help="define output path", type=str)
 
     args = parser.parse_args()
 
-    if args.data_path != args.out_path:
-        tr = train(args.data_path, args.out_path)
+    if args.dp != args.op or args.dp == "def":
+        if (args.dp and args.op) == "def":
+            tr = train()
+        else:
+            tr = train(args.dp, args.op)
         tr.model_prep()
+        tr.train_and_validate(epochs=5)
 
-        tr.train_and_validate(epochs=50)
+main()
--- scr/ConsoleApp1/Program.cs
+++ scr/ConsoleApp1/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace ConsoleApp1
 {
@@ -10,6 +12,42 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            execProcess();
+            Console.ReadKey();
+        }
+
+        static void execProcess(string pythonPath= "C:/Users/Ryzen7-EXT/anaconda3/envs/tensor/pythonw.exe", string scriptPath= "C:/Users/Ryzen7-EXT/Documents/Github/WickonHightech/resources/python/cli_example.py")
+        {
+            // create process info
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = pythonPath;
+
+            var dataPath = @"C:/Users/Ryzen7-EXT/Documents/Github/WickonHightech/resources/Assets";
+            var outPath = @"C:/Users/Ryzen7-EXT/Documents/Github/WickonHightech";
+
+            psi.Arguments = scriptPath + " " + dataPath + " " + outPath;
+
+
+            // process configuration
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+            psi.RedirectStandardOutput = true;
+            psi.RedirectStandardError = true;
+
+            string error;
+            string result;
+
+            using(var process = Process.Start(psi))
+            {
+                error = process.StandardError.ReadToEnd();
+                result = process.StandardOutput.ReadToEnd();
+            }
+
+            Console.WriteLine("Errors:");
+            Console.WriteLine(error);
+            Console.WriteLine("Res:");
+            Console.WriteLine(result);
+
         }
     }
 }
