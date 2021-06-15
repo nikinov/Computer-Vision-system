c63d845ed002a1727e4621f76a3671ccbe3482ba
nikinov <novellenicholas@gmail.com>
2021-06-15 12:58:14 +0200

    ...

--- resources/C++/build_sln.bat
+++ resources/C++/build_sln.bat
@@ -4,7 +4,5 @@
 cmake -DCMAKE_PREFIX_PATH=C:/Users/Ryzen7-EXT/Documents/Github/WickonHightech/resources/C++/Predictor/lib/libtorch
 
 :: CREATE PROJECT
-mkdir build
-cd build
 cmake -G "Visual Studio 16 2019" -A x64 -S . -B .\build
 pause
