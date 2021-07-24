330c3ae3bfbb3d7e875b9d65cf0ec06a07d0a242
nikinov <novellenicholas@gmail.com>
2021-07-24 20:42:13 +0200

    Update ocr_test.py

--- resources/python/prototying/ocr_test.py
+++ resources/python/prototying/ocr_test.py
@@ -6,19 +6,20 @@ import glob
 reader = easyocr.Reader(['en'], gpu=False)
 data_dir = "../../Assets5082"
 
-for entry in glob.iglob(data_dir + '/**/*.bmp', recursive=True):
-    prediction = reader.readtext(io.imread(entry))
-    print(prediction)
-
-def reading(reader):
+def reading():
     num_correct = 0
     num_incorrect = 0
     for entry in glob.iglob(data_dir + '/**/*.bmp', recursive=True):
-        print(entry + " Prediction: ")
-
         prediction = reader.readtext(io.imread(entry))
-        if prediction == entry.split("\\")[-2]:
+        try:
+            prediction = list(prediction[0])[1]
+        except:
+            prediction = 'E'
+
+        if str(prediction) == entry.split("/")[-2]:
             num_correct += 1
         else:
             num_incorrect += 1
     print("number of correct: " + str(num_correct) + " number of incorrect: " + str(num_incorrect))
+
+reading()
