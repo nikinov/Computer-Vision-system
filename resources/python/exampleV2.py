6c930277de38321b8298f964cc33e0e179998e85
nikinov <novellenicholas@gmail.com>
2021-09-01 13:03:12 +0200

    cleanup

--- resources/python/exampleV2.py
+++
@@ -1,4 +0,0 @@
-from resources.python.train.trainV2 import train
-
-
-tr = train(use_config=False, grayscale=True)
--- resources/python/train/fast_ai_test.py
+++
@@ -1,9 +0,0 @@
-from fastai.vision import *
-from DataLoaders import FolderDataset
-import fastai.datasets
-
-dataset_path = "../../Assets5082"
-
-dls = ImageDataBunch.from_folder(dataset_path, )
-
-learner = cnn_learner()
