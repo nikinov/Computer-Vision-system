0a32d35aa42c8e65ce97e9c73511b71565f2b275
nikinov <novellenicholas@gmail.com>
2021-09-14 15:41:28 +0200

    plotting

---
+++ resources/python/plot/plot.py
@@ -0,0 +1,58 @@
+from matplotlib import pyplot as plt
+from shapely.geometry.polygon import LinearRing, Polygon
+from shapely.ops import cascaded_union
+
+
+path = "C:/maNiceOne.txt"
+f = open(path)
+
+def plot_lines():
+    all_lines = []
+    lines = f.read().split("|\n")
+    for line in lines:
+        l = []
+        for ll in line.split("|"):
+            try:
+                l.append(ll.split(","))
+            except:
+                pass
+        if l is not None:
+            all_lines.append(l)
+
+
+    for lin in all_lines:
+        try:
+            x_values = [lin[0][0], lin[1][0]]
+
+            y_values = [lin[0][1], lin[1][1]]
+
+            plt.plot(x_values, y_values)
+        except:
+            pass
+
+def plot_polygon():
+    fig,ax = plt.subplots(1)
+
+    new_coords = []
+
+    for line in f.read().split(",\n"):
+        try:
+            coords = []
+            for lin in line.split(","):
+                try:
+                    li = lin.split("|")
+                    coords.append([float(li[0]), float(li[1])])
+                except:
+                    pass
+            coords.append(coords[0])
+            new_coords.append(coords)
+        except:
+            pass
+
+    for cor in new_coords:
+        xs, ys = zip(*cor)
+        fig = plt.figure()
+        plt.plot(xs, ys)
+
+plot_polygon()
+plt.show()
---
+++ resources/python/plot/plot_test.py
@@ -0,0 +1,49 @@
+from matplotlib import pyplot as plt
+from descartes import PolygonPatch
+from shapely.geometry import Polygon, LinearRing
+from shapely.ops import cascaded_union
+
+
+
+# define the two polygons to merge
+polygon1 = Polygon([(10, 3), (0, 0), (5, 0)])
+polygon2 = Polygon([(0, 0), (3, 7), (3, 0)])
+polygons = [polygon1, polygon2]
+
+
+# plot these two polygons separately
+fig = plt.figure(1, figsize=(10,10), dpi=90)
+ax = fig.add_subplot(111)
+poly1patch = PolygonPatch(polygon1, alpha=0.5, zorder=2)
+poly2patch = PolygonPatch(polygon2, alpha=0.5, zorder=2)
+ax.add_patch(poly1patch)
+ax.add_patch(poly2patch)
+xrange = [-2, 12]
+yrange = [-2, 8]
+ax.set_xlim(*xrange)
+ax.set_xticks(range(*xrange) + [xrange[-1]])
+ax.set_ylim(*yrange)
+ax.set_yticks(range(*yrange) + [yrange[-1]])
+ax.set_aspect(1)
+
+#my_ring = LinearRing(u.exterior.coords)
+#print(my_ring.is_ccw)
+
+# Make the merged polygons
+u = cascaded_union(polygons)
+
+# Make new figure for the merged polygon
+fig2 = plt.figure(2, figsize=(10,10), dpi=90)
+ax2 = fig2.add_subplot(111)
+patch2b = PolygonPatch(u, alpha=1, zorder=2)
+ax2.add_patch(patch2b)
+
+xrange = [-2, 12]
+yrange = [-2, 8]
+ax2.set_xlim(*xrange)
+ax2.set_xticks(range(*xrange) + [xrange[-1]])
+ax2.set_ylim(*yrange)
+ax2.set_yticks(range(*yrange) + [yrange[-1]])
+ax2.set_aspect(1)
+
+fig2.show(2)
